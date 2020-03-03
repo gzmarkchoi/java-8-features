974dc68910bfe77847f0806cf353fb27b0041939
gzmarkchoi <gzmarkchoi@gmail.com>
2020-03-03 17:46:07 +0100

    - Stream numeric operations:
    - sum()
    - range()
    - rangeClosed()
    - max()
    - min()
    - average()
    - boxed()
    - mapToInt()
    - mapToLong
    - mapToObj()
    - mapToDouble()

---
+++ src/com/learn/Java/numericstreams/NumericStreamRangesExample.java
@@ -0,0 +1,29 @@
+package com.learn.Java.numericstreams;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class NumericStreamRangesExample {
+    public static void main(String[] args) {
+        // It stops at 49
+        IntStream intStream = IntStream.range(1, 50);
+        System.out.println(intStream.count());
+
+        System.out.println("Range count " + IntStream.range(1, 50).count());
+        IntStream.range(1, 50).forEach((value -> System.out.print(value + ",")));
+
+        System.out.println();
+
+        System.out.println("Range closed count " + IntStream.rangeClosed(1, 50).count());
+        IntStream.rangeClosed(1, 50).forEach(value -> System.out.print(value + ","));
+
+        System.out.println();
+        System.out.println("Long stream closed count " + LongStream.rangeClosed(1, 50).count());
+        LongStream.rangeClosed(1, 50).forEach(value -> System.out.print(value + ","));
+
+        System.out.println();
+        System.out.println("Double stream closed count " + IntStream.rangeClosed(1, 50).asDoubleStream().count());
+        IntStream.range(1, 50).asDoubleStream().forEach(value -> System.out.print(value + ","));
+    }
+}
